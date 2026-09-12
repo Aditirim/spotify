66a1c753849aec7b48a72f80e0e9f7c6e43beb9e
Aditi Pandey <aditirim2006@gmail.com>
2026-09-13 00:08:44 +0530

    added auth controller

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,45 @@
+const userModel=require('../models/user.model');
+
+const jwt=require('jsonwebtoken');
+
+const registerUser=async (req, res) => {
+    const {username, email, password, role = 'user'} = req.body;
+    const isUserAlreadyExist = await userModel.findOne({
+        $or: [
+            { email },
+            { username }
+        ]
+    });
+    if (isUserAlreadyExist) {
+        return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const user = await userModel.create({
+        username,
+        email,
+        password,
+        role
+    });
+
+    const token = jwt.sign({
+        id: user._id,
+        role: user.role
+    }, process.env.JWT_SECRET);
+
+    res.cookie('token', token)
+
+    res.status(201).json({
+
+        message: 'User registered successfully',
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role,
+        }
+    });
+
+};
+
+
+module.exports={registerUser};
