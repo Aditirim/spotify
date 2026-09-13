fabb31de8aebe6e9af26b915befb564167e65ba4
Aditi Pandey <aditirim2006@gmail.com>
2026-09-14 00:54:40 +0530

    added password hashing & login auth logic

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const userModel=require('../models/user.model');
 
 const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
 
 const registerUser=async (req, res) => {
     const {username, email, password, role = 'user'} = req.body;
@@ -14,10 +15,11 @@ const registerUser=async (req, res) => {
         return res.status(400).json({ message: 'User already exists' });
     }
 
+    const hash = await bcrypt.hash(password, 10);
     const user = await userModel.create({
         username,
         email,
-        password,
+        password: hash,
         role
     });
 
@@ -41,5 +43,42 @@ const registerUser=async (req, res) => {
 
 };
 
+const loginUser=async (req,res)=>{
+    const {username,email,password}=req.body;
+    const user=await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    });
+    if(!user){
+        return res.status(400).json({message:'User not found'});
+    }
+    const  isValidPassword=await bcrypt.compare(password,user.password);
 
-module.exports={registerUser};
+    if(!isValidPassword){
+        return res.status(401).json({message:'Invalid password'});
+    }
+
+    const token=jwt.sign({
+        id:user._id,
+        role:user.role
+    },process.env.JWT_SECRET);
+
+    res.cookie('token',token);
+
+    res.status(200).json({
+        message:'User logged in successfully',
+        user:{
+            id:user._id,
+            username:user.username,
+            email:user.email,
+            role:user.role
+        }
+    });
+
+}
+
+
+
+module.exports={registerUser,loginUser};
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,7 +1,10 @@
 const express=require('express');
 
 const router=express.Router();
+const authController=require('../controllers/auth.controller');
 
-router.post('/register');
+router.post('/register',authController.registerUser);
+
+router.post('/login',authController.loginUser);
 
 module.exports=router;
